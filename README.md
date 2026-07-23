0ee3564a24c4927c51eba51810a0df4a668d8c38
Aleem-27 <aleemkhan2405@gmail.com>
2026-07-23 20:30:53 +0500

    chore(chat-application): setup the initials, packages, folder structure, and add a dbcontext class

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat-Application
+
+Converseo is a chatting-application bullt using SignalR with ASP.NET and ReactJS, production standards with potential to use AI features in future
---
+++ server/ChatApp.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Api.Data;
+
+public class AppDbContext : DbContext
+{
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+  {
+
+  }
+}
