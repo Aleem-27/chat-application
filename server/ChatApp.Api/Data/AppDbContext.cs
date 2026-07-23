3baea37b8f34991702c398858227ae1588d79c23
Aleem-27 <aleemkhan2405@gmail.com>
2026-07-23 21:18:11 +0500

    feat(data): inherit from identitydbcontext, create a refresh token dbset and add unique index on it

--- server/ChatApp.Api/Data/AppDbContext.cs
+++ server/ChatApp.Api/Data/AppDbContext.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ChatApp.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChatApp.Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
   public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
   {
+  }
 
+  public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+  protected override void OnModelCreating(ModelBuilder builder)
+  {
+    base.OnModelCreating(builder);
+
+    builder.Entity<RefreshToken>()
+      .HasIndex(rt => rt.Token)
+      .IsUnique();
   }
 }
