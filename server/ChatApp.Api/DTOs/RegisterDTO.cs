a7abd981a04fad45e345d4b04d8a357881d7a1c2
Aleem-27 <aleemkhan2405@gmail.com>
2026-07-25 17:21:45 +0500

    feat(dto): create login, register and user response DTOs

---
+++ server/ChatApp.Api/DTOs/RegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Api.DTOs;
+
+public class RegisterDTO
+{
+  [Required, EmailAddress]
+  public string Email { get; set; } = string.Empty;
+
+  [Required, MinLength(8)]
+  public string Password { get; set; } = string.Empty;
+
+  [Required, MaxLength(50)]
+  public string DisplayName { get; set; } = string.Empty;
+}
