3c937d803ebb604fb7e20a777f0c6b663510e021
Aleem-27 <aleemkhan2405@gmail.com>
2026-07-26 18:49:45 +0500

    chore(client): tighten ESLint to type-checked rules and add Prettier for formatting

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,7 +12,7 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -19,4 +20,6 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+
+  prettier,
 ])
