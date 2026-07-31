bd96c695bd164571dc102eb76a3aa56b87ea5e2f
Aleem-27 <aleemkhan2405@gmail.com>
2026-07-31 19:36:00 +0500

    feat(dto): add profile update and change password DTOs

---
+++ server/ChatApp.Api/DTOs/ChangePasswordDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Api.DTOs;
+
+public class ChangePasswordDTO
+{
+  [Required]
+  public string CurrentPassword { get; set; } = string.Empty;
+
+  [Required, MinLength(8)]
+  public string NewPassword { get; set; } = string.Empty;
+}
---
+++ server/ChatApp.Api/DTOs/UpdateProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Api.DTOs;
+
+public class UpdateProfileDTO
+{
+  [MaxLength(50)]
+  public string? DisplayName { get; set; }
+
+  [EmailAddress]
+  public string? Email { get; set; }
+
+  public string? AvatarUrl { get; set; }
+}
